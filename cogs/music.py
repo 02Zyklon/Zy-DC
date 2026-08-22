777b15bba3a92fcd208789faaf89278f6730bf48
Zyklon.ofc <145393626+02Zyklon@users.noreply.github.com>
2026-08-21 22:18:00 -0300

    Update Lavalink node password

--- cogs/music.py
+++ cogs/music.py
@@ -20,7 +20,7 @@ class Music(commands.Cog):
         node = wavelink.Node(
             identifier="ZyklonRenderNode",
             uri="https://lavalink-qvir.onrender.com",
-            password="$1N;ZyklonSS"
+            password="youshallnotpass"
         )
         try:
             await wavelink.Pool.connect(nodes=[node], client=self.bot)
